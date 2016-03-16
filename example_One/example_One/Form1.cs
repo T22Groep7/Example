47ec85e0b17064da8b94ad2e0a27b958fe3987cb
charmesal <kevindullens3@hotmail.com>
2016-03-16 10:58:10 +0100

    subject

---
+++ example_One/example_One/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace example_One
+{
+    public partial class Form1 : Form
+    {
+        int Bas, John, Kevin, Niels, Robert, Sanders;
+        public Form1()
+        {
+            Bas = 1;
+            John = 1;
+            Kevin = 1;
+            Niels = 1;
+            Robert = 1;
+            Sanders = 1;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
